a5f57eca11837fb47c4c30ca75057602dffbfc60
j3-hardwick <james3.hardwick@live.uwe.ac.uk>
2022-05-02 15:56:13 +0100

    preproccessing

--- test.py
+++ test.py
@@ -1,13 +1,21 @@
 import pandas as pd
 import re
+import sklearn
 
 df = pd.read_csv('data/good_badnews_with_text.csv')
 text = df['text']
 y = df['label']
 
 tweets = []
+words = set()
 
 for tweet in text:
-    tweets.append(re.sub(r'https?://\S+', '', tweet).strip().replace('#','').replace(',',''))
+    tweet_text = re.sub(r'https?://\S+', '', tweet).strip()
+    tweet_text = ''.join([x.lower() for x in tweet_text if x.isalpha() or x == ' '])
+    tweets.append(tweet_text)
+    tweet_words = [x.lower() for x in tweet_text.split(' ')]
+    words = words.union(set(tweet_words))
+
+print('Number of words in the corpus:',len(words))
 
 print()
