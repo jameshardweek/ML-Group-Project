7e1405f74a82e39e90e7dd55f0f3db55fcb004e2
j3-hardwick <james3.hardwick@live.uwe.ac.uk>
2022-05-03 15:01:14 +0100

    attempt classification for given sentance

--- tfidf_nb_classifier.py
+++ tfidf_nb_classifier.py
@@ -11,14 +11,12 @@ y = [x for x in pd.read_csv('data/processed.csv')['status']]
 td  = TfidfVectorizer()
 X = td.fit_transform(tweets).toarray()
 
-feature_names = [x for x in td.get_feature_names_out()]
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3)
 
-classifier = MultinomialNB()
-classifier.fit(X_train, y_train)
+clf = MultinomialNB()
+clf.fit(X_train, y_train)
 
-y_pred = classifier.predict(X_test)
+y_pred = clf.predict(X_test)
 
 classification_report = classification_report(y_test, y_pred)
 
@@ -29,3 +27,15 @@ print(f'Classification Report:')
 print(f'======================================================')
 print(f'{classification_report}')
 print(f'======================================================')
+
+
+df = pd.DataFrame(X, columns = td.get_feature_names_out())
+
+test_sentence = 'man cured of disease'
+test_status = 1
+
+test_X = td.transform(test_sentence.split(' ')).toarray()
+
+test_predict = clf.predict(test_X)
+
+score = accuracy_score([test_status], test_predict)
